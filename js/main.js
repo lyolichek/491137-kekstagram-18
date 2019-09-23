30effee18a6a57361facf1a29d8e6da00b0341c3
olga iarmak <olga.iarmak@ix.co>
2019-09-24 00:08:11 +0300

    module3-task3

--- js/main.js
+++ js/main.js
@@ -2,21 +2,20 @@
 
 var QUANTITY = 25;
 
-var commentsParams = {
-  COMMENTS: [
+var comments =  [
     'Всё отлично!',
     'В целом всё неплохо. Но не всё.',
     'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
     'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
     'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ],
-  NAMES: ['Лёля', 'Слава', 'Аня', 'Ваня', 'Елена', 'Вова']
-};
+  ];
+var names = ['Лёля', 'Слава', 'Аня', 'Ваня', 'Елена', 'Вова'];
 
 var pictures = document.querySelector('.pictures');  //Контейнер для изображений от других пользователей
-var template = document.querySelector('#picture').content.querySelector('.picture');  // template
+var template = document.querySelector('#picture').content.querySelector('.picture');  // содержимое тега template
 var fragment = document.createDocumentFragment();
+var bigPictureOverlay = document.querySelector('.big-picture');
 
 var teplateArray = createDataArray();
 
@@ -41,7 +40,7 @@ function createDataObject(i) {
   return {
     src: 'photos/' + i + '.jpg',
     likes: randomInteger(5, 200),
-    comments: generateComments(commentsParams.COMMENTS)
+    comments: generateComments(comments)
   }
 }
 
@@ -49,6 +48,7 @@ function createDataObject(i) {
 function generateComments(array) {
   var number = randomInteger(0, 5);
   var newArray = [];
+
   for(var i = 0; i <= number; i++){
     newArray.push(array[i]);
   }
@@ -73,9 +73,55 @@ function createFragment(obj) {
 // создание DOM-элементов, соответствующие фотографиям и заполните их данными из массива
 function createElements(arrayElements) {
   for(var i = 0; i < arrayElements.length; i++) {
+
+    if(i === 0) {
+      openBigPictureOverlay(arrayElements[i]);
+    }
+
     fragment.appendChild(createFragment(arrayElements[i]));
   }
 }
 
+// создание полноэкранного показа изображения
+function openBigPictureOverlay(obj) {
+  var bigPicture = bigPictureOverlay.querySelector('.big-picture__img img');
+  var bigPictureLikes = bigPictureOverlay.querySelector('.likes-count');
+  var bigPictureCommentsCount = bigPictureOverlay.querySelector('.comments-count');
+  var bigPictureComments = bigPictureOverlay.querySelector('.social__comments'); // ul
+
+  bigPicture.setAttribute('src', obj.src);
+  bigPictureLikes.textContent = obj.likes;
+  bigPictureCommentsCount.textContent = obj.comments.length;
+  bigPictureComments.innerHTML = createSocialCommentsBlock(obj.comments); // 2 comments
+}
+
+// создание блока с аватаром и комментами
+function createSocialCommentsBlock(array) {
+  var newArray = [];
+  for(var i = 0; i < array.length; i++) {
+    var item = createObjectCommentBlock(array[i], i, names); //вызываем ф-ю создания li с контентом
+    newArray.push(item);
+    console.log(newArray)
+  }
+
+  return newArray; // вернет обект с 2 значениями
+}
+
+// формируем объект блока комментов
+function createObjectCommentBlock(text, i, arr) {
+  var commentItem = document.querySelector('.social__comment');
+  var cloneElement = commentItem.cloneNode(true);
+  var socialPicture = cloneElement.querySelector('.social__picture');
+  var socialText = cloneElement.querySelector('.social__text');
+
+  socialPicture.setAttribute('src', 'img/avatar-' + randomInteger(i + 1, 6) + '.svg');
+  socialPicture.setAttribute('alt', arr[i]);
+  socialText.textContent = text;
+
+  //console.log(i)
+  return commentItem;
+}
+
 createElements(teplateArray);
 pictures.appendChild(fragment);
+bigPictureOverlay.classList.remove('hidden');
