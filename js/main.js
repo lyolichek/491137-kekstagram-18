bb9e50670d817783426a449939b112098f5b5033
olga iarmak <olga.iarmak@ix.co>
2019-09-30 02:14:51 +0300

    module4-task2 - created styles for images

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 'use strict';
 
 var QUANTITY = 25;
+var FILTERS = {
+  'effect-none': '',
+  'effect-chrome': 'effects__preview--chrome',
+  'effect-sepia': 'effects__preview--sepia',
+  'effect-marvin': 'effects__preview--marvin',
+  'effect-phobos': 'effects__preview--phobos',
+  'effect-heat': 'effects__preview--heat'
+};
 
 var comments = [
   'Всё отлично!',
@@ -16,6 +24,7 @@ var pictures = document.querySelector('.pictures');
 var template = document.querySelector('#picture').content.querySelector('.picture');
 var fragment = document.createDocumentFragment();
 var bigPicture = document.querySelector('.big-picture');
+var bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
 var bigPictureComments = document.querySelector('.social__comments');
 var bigPictureComment = document.querySelector('.social__comment');
 
@@ -123,3 +132,85 @@ function deleteDefaultComments(listOfComments) {
 createElements(teplateArray);
 pictures.appendChild(fragment);
 
+bigPictureCancel.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  bigPicture.classList.add('hidden');
+})
+
+
+
+// Загрузка нового изображения
+var uploadFile = document.querySelector('#upload-file');
+var uploadOverlay = document.querySelector('.img-upload__overlay');
+var uploadOverlayCancel = uploadOverlay.querySelector('.img-upload__cancel');
+
+uploadFile.addEventListener('change', function() {
+  uploadOverlay.classList.remove('hidden');
+});
+
+uploadOverlayCancel.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  uploadOverlay.classList.add('hidden');
+});
+
+// Увеличение масштаба изображения
+var STEP = 25;
+var imageUploadScale = document.querySelector('.img-upload__scale');
+var scaleControlValue = imageUploadScale.querySelector('.scale__control--value');
+var buttonSmall = imageUploadScale.querySelector('.scale__control--smaller');
+var buttonlBig = imageUploadScale.querySelector('.scale__control--bigger');
+var imageUploadPreview = document.querySelector('.img-upload__preview');
+
+scaleControlValue.setAttribute('value', '100%');
+
+var currentValue = parseInt(scaleControlValue.getAttribute('value'), 10);
+
+buttonSmall.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  changeValue(currentValue, false);
+});
+
+buttonlBig.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  changeValue(currentValue, true);
+});
+
+function changeValue(value, isGrow) {
+  if (!isGrow && value > STEP) {
+    value -= STEP;
+  } else if (isGrow && value < 100) {
+    value += STEP;
+  }
+  resizeImage(value);
+  currentValue = value;
+  scaleControlValue.setAttribute('value', value + '%');
+}
+
+function resizeImage(value) {
+  imageUploadPreview.style.transform = 'scale(' + value / 100 + ')';
+}
+
+// Наложение эффекта на изображение
+var imageUploadEffects = document.querySelector('.img-upload__effects');
+var effectsItems = imageUploadEffects.querySelectorAll('.effects__item');
+
+for (var i = 0; i < effectsItems.length; i++) {
+  addThumbnailClickHandler(effectsItems[i]);
+}
+
+function addThumbnailClickHandler(thumbnail) {
+  thumbnail.addEventListener('click', function() {
+    var item = thumbnail.querySelector('.effects__label');
+    var filterName = item.getAttribute('for');
+    var picture = imageUploadPreview.querySelector('img');
+    picture.removeAttribute('class');
+    picture.classList.add(FILTERS[filterName]);
+  })
+}
+
+//Интенсивность эффекта регулируется перемещением ползунка в слайдере
+var uploadEffectLevelPin = document.querySelector('.effect-level__pin');  // ползунок
+
+//uploadEffectLevelPin.addEventListener('mouseup', function () {
+//  alert('hello');
+//});
