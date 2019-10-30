0b2368c87147e1632487015df9481214516a0f98
olga.iarmak <olga.iarmak@ix.co>
2019-10-31 00:27:47 +0200

    fixed for the first checking

--- js/upload-photo.js
+++ js/upload-photo.js
@@ -37,6 +37,7 @@
   window.blockPictures = document.querySelector('.pictures');
   window.errorBlock = document.querySelector('#error').content.querySelector('.error');
   window.buttonError = errorBlock.querySelectorAll('.error__button');
+  
 
   uploadFile.addEventListener('change', function () {
     window.popup.open(uploadOverlay);
